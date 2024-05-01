29df2387894dc3f8a0e2c30b6fec86b75786ad64
berkan <berkanyasinalpay@gmail.com>
2024-05-01 21:00:30 +0300

    Navigation bar and routers added

---
+++ lib/components/customNavBar.dart
@@ -0,0 +1,50 @@
+import 'package:budgetbuddy/view/home/main-page/view/receiptPage.dart';
+import 'package:flutter/material.dart';
+import '../view/home/main-page/view/financePage.dart';
+import '../view/home/main-page/view/mainPage.dart';
+
+class CustomNavbar extends StatefulWidget {
+  const CustomNavbar({super.key});
+
+  @override
+  State<CustomNavbar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<CustomNavbar> {
+  int _selectedIndex=1;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    if (index == 0) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => FinancePage()));
+    } else if (index == 1) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
+    } else if (index == 2) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => ReceiptPage()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        currentIndex: _selectedIndex, // Şu anki seçili indeksi göstermek için kullanılır
+        onTap: _onItemTapped, // İşlevi atama
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            label: 'Gelir-Gider',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt,),
+            label: 'Faturalarım',
+          ),
+        ],
+    );
+  }
+}
