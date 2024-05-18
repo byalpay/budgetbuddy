2d80de1707e3c60be5895f6c4c3d8a5bf5f92ca0
berkan <berkanyasinalpay@gmail.com>
2024-05-18 22:03:17 +0300

    Button renamed

--- lib/components/authButton.dart
+++ lib/components/authButton.dart
@@ -1,32 +1,26 @@
 import 'package:flutter/material.dart';
-import '../view/home/main-page/view/mainPage.dart';
 
 class LoginButton extends StatelessWidget {
-  const LoginButton({super.key});
+  final VoidCallback onPressed;
+  String text;
 
-
+  LoginButton({required this.onPressed, Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 25),
       child: ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MainPage()),
-          );
-        },
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(horizontal: 80,vertical: 20),
-          // buradan yatay dikey büyüt
+          padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
           backgroundColor: Colors.black54,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), // Köşeleri kırpma
+            borderRadius: BorderRadius.circular(20),
           ),
         ),
-        child: const Text(
-          "Giriş Yap",
+        child: Text(
+          text,
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
