fc87025059cc28ba01e716fc8ce5ecfe7c647d7d
berkan <berkanyasinalpay@gmail.com>
2024-05-19 14:34:06 +0300

    Receipt page functions added

--- lib/components/customReceiptDialog.dart
+++ lib/components/customReceiptDialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../controllers/receipts/addReceipts.dart';
 import 'customField.dart';
 
 class CustomReceiptDialog extends StatefulWidget {
@@ -9,44 +9,98 @@ class CustomReceiptDialog extends StatefulWidget {
   State<CustomReceiptDialog> createState() => _CustomReceiptDialogState();
 }
 
-var receiptName;
-var amount;
-var dueDate;
-var status;
-
-TextEditingController receiptNameController = TextEditingController();
-TextEditingController amountController = TextEditingController();
-TextEditingController dueDateController = TextEditingController();
-
 class _CustomReceiptDialogState extends State<CustomReceiptDialog> {
+  final TextEditingController receiptNameController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+  final TextEditingController dueDateController = TextEditingController();
+
+  bool isPaid = false;  // Switch durumunu takip eden değişken
+
+  void addReceiptsBack() {
+    int amount = int.tryParse(amountController.text.trim()) ?? 0;  // String'i int'e dönüştür
+    DateTime? dueDate = DateTime.tryParse(dueDateController.text.trim());  // String'i DateTime'a dönüştür
+
+    if (dueDate == null) {
+      dueDate = DateTime.now();  // Hatalı tarih girişi olursa, varsayılan olarak bugünün tarihini kullanır
+    }
+
+    addReceiptDetails(
+      receiptNameController.text.trim(),
+      amount,
+      dueDate,
+      isPaid,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text("Fatura Kaydı"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min, // Dialog içinde minimum yükseklik
-        children: [
-          CustomField(
-            controller: receiptNameController,
-            hintText: "Fatura Adı",
-            key: GlobalKey(),
-          ),
-          SizedBox(height: 10),
-          CustomField(
-            controller: amountController,
-            hintText: "Miktar",
-            key: GlobalKey(),
-          ),
-          TextField(
-            controller: dueDateController,
-            decoration: InputDecoration(labelText: 'Son Ödeme Tarihi'),
-          ),
-        ],
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomField(
+              labelText: "Fatura Adı",
+              controller: receiptNameController,
+              hintText: "Fatura Adı",
+              key: GlobalKey(),
+            ),
+            SizedBox(height: 10),
+            CustomField(
+              labelText: "Miktar",
+              controller: amountController,
+              hintText: "Miktar",
+              keyboardType: TextInputType.number,  // Sadece sayısal giriş için klavye tipi
+              key: GlobalKey(),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: dueDateController,
+              decoration: InputDecoration(
+                labelText: 'Son Ödeme Tarihi',
+                border: OutlineInputBorder(),
+              ),
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+                if (pickedDate != null) {
+                  setState(() {
+                    dueDateController.text =
+                    "${pickedDate.toLocal()}".split(' ')[0];
+                  });
+                }
+              },
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Ödenmedi"),
+                Switch(
+                  value: isPaid,
+                  onChanged: (value) {
+                    setState(() {
+                      isPaid = value;
+                    });
+                  },
+                ),
+                Text("Ödendi"),
+              ],
+            ),
+          ],
+        ),
       ),
       actions: [
         ElevatedButton(
-          onPressed: () => Navigator.of(context).pop(),
-          // Dialog'u kapatır, Kaydetme işlemi burada gerçekleşecek...
+          onPressed: () {
+            addReceiptsBack();
+            Navigator.of(context).pop();  // Dialog'u kapatır
+          },
           child: Text("Faturayı Kaydet"),
         ),
       ],
